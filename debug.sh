231a7061de779169931e85dfe2c66e9c5b10aa71
sidpixel <45841651+sidpixel@users.noreply.github.com>
2023-09-06 11:53:53 +0800

    Update debug.sh

--- debug.sh
+++ debug.sh
@@ -1,6 +1,6 @@
 sudo swapoff /mnt/swapfile       
 sudo rm -rf /imagegeneration/installers /opt/az /opt/hostedtoolcache /opt/microsoft /opt/pipx /opt/googles /mnt/swapfile 
 #sudo    du -sh /home/* /imagegeneration/* /opt/* /mnt/*
-sudo du -sh /*
+sudo du -sh /usr/* /home/*
 df -hT
 
