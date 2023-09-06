8e16268548c7b251ba7dca694295072b2a815f66
sidpixel <45841651+sidpixel@users.noreply.github.com>
2023-09-06 12:13:24 +0800

    Update debug.sh

--- debug.sh
+++ debug.sh
@@ -1,6 +1,6 @@
 sudo swapoff /mnt/swapfile       
 #sudo rm -rf /imagegeneration/installers /opt/az /opt/hostedtoolcache /opt/microsoft /opt/pipx /opt/googles /mnt/swapfile 
-sudo rm -rf /imagegeneration /opt /mnt/swapfile /usr/local/aws*  /usr/local/julia* /usr/share/swift* /usr/share/sbt*
+sudo rm -rf /imagegeneration /opt /mnt/swapfile /usr/local/aws*  /usr/local/julia* /usr/share/swift* /usr/share/sbt* /usr/local/share/chromium*  /usr/local/share/powershell*
 
 #sudo    du -sh /home/* /imagegeneration/* /opt/* /mnt/* /usr/share/miniconda*  /usr/share/dotnet* /usr/share/az* /usr/local/sqlpackage*  
 sudo du -sh   /usr/local/share/*
